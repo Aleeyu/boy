affc5b6d22fa0bdc38931debecea65c07957224a
liyu <yli@erathink.com>
2016-11-23 20:49:19 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/home/controller/index.js
    #	view/home/index_index.html
    首页

---
+++ www/static/js/index.js
@@ -0,0 +1,6 @@
+
+/**
+ * Created by 75339 on 2016/11/23.
+ */
+console.log($(".list-ul-item").width()*$(".piclistbox>li").length)
+$(".piclistbox").width(($(".list-ul-item").width()+10)*$(".piclistbox>li").length)
